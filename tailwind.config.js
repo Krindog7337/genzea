306f6e655b8eaaa36a3715cd06a3c8ca04f2c779
Krindog7337 <igstkrisna2006@gmail.com>
2023-04-21 23:56:14 +0800

    Hero Section and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ceria-light": "#f2e3c6",
+        "ceria-cream": "#ffc6a5",
+        "ceria-accent": "#e6324b",
+        "ceria-text": "#2b2b2b",
+        "ceria-dark": "#353634",
+      },
+    },
   },
   plugins: [],
-}
-
+};
