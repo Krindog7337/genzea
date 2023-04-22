856d2b5825fd39cabd66285d069de3069a6702f2
Krindog7337 <igstkrisna2006@gmail.com>
2023-04-22 16:10:12 +0800

    clamp the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+        '8xl': '6rem',
+        '9xl': '7rem',
+        'step0': ['clamp(1.00rem, calc(0.96rem + 0.22vw), 1.13rem)'],
+      },
       colors: {
         "ceria-light": "#f2e3c6",
         "ceria-cream": "#ffc6a5",
@@ -10,6 +26,25 @@ module.exports = {
         "ceria-text": "#2b2b2b",
         "ceria-dark": "#353634",
       },
+      fontFamily: {
+        sans: [
+        'Plus Jakarta Sans', 
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      }
     },
   },
   plugins: [],
