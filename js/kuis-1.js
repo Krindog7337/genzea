d6b3813f72ec27dfe6b6017f4b94730bad4139fd
Krindog7337 <igstkrisna2006@gmail.com>
2023-04-23 14:16:58 +0800

    quiz-1 page

---
+++ js/kuis-1.js
@@ -0,0 +1,39 @@
+var form = document.getElementById("form-kuis");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  calculateScore();
+});
+
+function calculateScore() {
+  var score = 0;
+  var answers = ["a", "c", "c", "a", "b"]; // array of correct answers
+
+  var q1 = document.forms["form-kuis"]["q1"].value;
+  var q2 = document.forms["form-kuis"]["q2"].value;
+  var q3 = document.forms["form-kuis"]["q3"].value;
+  var q4 = document.forms["form-kuis"]["q4"].value;
+  var q5 = document.forms["form-kuis"]["q5"].value;
+
+  if (q1 == answers[0]) {
+    score++;
+  }
+
+  if (q2 == answers[1]) {
+    score++;
+  }
+
+  if (q3 == answers[2]) {
+    score++;
+  }
+
+  if (q4 == answers[3]) {
+    score++;
+  }
+
+  if (q5 == answers[4]) {
+    score++;
+  }
+
+  var scoreDisplay = document.getElementById("scoreDisplay");
+  scoreDisplay.innerHTML = "Anda benar " + score + " dari " + answers.length;
+}
