d094dae49791d287d7fa575700f0e8b64950defd
krindog7337 <igstkrisna2006@gmail.com>
2024-10-27 16:23:21 +0800

    remove unused JS

--- js/navigation.js
+++
@@ -1,6 +0,0 @@
-const navButton = document.querySelector("button.mobile-menu-button");
-const navMenu = document.querySelector(".mobile-navigation");
-
-navButton.addEventListener("click", () => {
-  navMenu.classList.toggle("hidden");
-});
